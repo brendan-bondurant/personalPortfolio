f5cc85de8b84cfc7ba01084e571980c27c1fc48e
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-10-28 12:10:04 -0700

    updates to switch cpu

---
+++ MyPortfolio/Shared/BonsaiBid.razor.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Components;
+
+namespace BonsaiBid.Shared
+{
+      public partial class BonsaiBid : ComponentBase
+    {
+    // List of tech logo image paths
+    private List<string> TechLogos = new List<string>
+    {
+        "images/Icons/Logos/AWS.png",
+        "images/Icons/Logos/Azure.png",
+        "images/Icons/Logos/Bootstrap.png",
+        "images/Icons/Logos/C#.png",
+        "images/Icons/Logos/CSS.png",
+        "images/Icons/Logos/HTML.png",
+        "images/Icons/Logos/JS.png",
+        "images/Icons/Logos/Rails.png"
+        // Add more as needed
+    };
+    }
+}
+
