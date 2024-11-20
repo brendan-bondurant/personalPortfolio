9f716fcebedb75bac7dd413349b05f44fd2c36cb
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-11-20 10:57:52 -0800

    Content is there, needs some editing

---
+++ MyPortfolio/Shared/Portfolio.razor.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace MyPortfolio.Shared
+{
+    public partial class Portfolio : ComponentBase
+    {
+        protected List<string> TechLogos { get; set; }
+
+        protected override void OnInitialized()
+        {
+            TechLogos = new List<string>
+            {
+                "images/Logos/Csharp.png",
+                "images/Logos/Blazor.png",
+                "images/Logos/Bootstrap.png",
+                "images/Logos/github.png",
+                "images/Logos/CSS.png",
+                "images/Logos/HTML.png",
+                "images/Logos/Azure.png"
+            };
+        }
+    }
+}
+
